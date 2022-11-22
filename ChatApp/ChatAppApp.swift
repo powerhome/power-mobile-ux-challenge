8e7aeb8aee3f8796ed50df0f789827b03c44a8f1
Jasper Furniss <jasper.furniss@powerhrg.com>
2022-11-22 14:47:01 -0500

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Jasper Furniss on 11/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
