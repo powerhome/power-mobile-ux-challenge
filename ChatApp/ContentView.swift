1864423d13989e7ce52f12a429b7898b58413b03
Jasper Furniss <jasper.furniss@powerhrg.com>
2022-12-12 12:57:15 -0500

    Put Hello World Text back after testing Avatar

--- ChatApp/ContentView.swift
+++ ChatApp/ContentView.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-          PBAvatar(image: .init(systemName: "person"), name: "Tim Wenhold", size: .medium, status: .away, shape: .circle)
-            Text("Hello, Tim!")
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
         .padding()
     }
